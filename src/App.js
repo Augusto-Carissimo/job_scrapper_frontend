f3936e5941a16a209a88a06ce15a61d074541813
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-02-28 09:38:36 -0300

    Fetch Positions from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Position from './Position.js'
 
 function App() {
+  const [allPosition, setAllPosition] = React.useState([])
+
+  React.useEffect(() => {
+    window.fetch('/position')
+      .then(response => response.json())
+      .then(data => setAllPosition(data))
+  }, [])
+
+  const positions = allPosition.map(position => {
+    return (
+      <Position
+        key={position.id}
+        {...position}
+      />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {positions}
     </div>
-  );
+  )
 }
 
 export default App;
