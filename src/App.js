8abae4f6b7aedf149a26c06dc7bc51262523b407
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-03-16 23:33:37 -0300

    Refresh page after scrapping

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import Position from './components/Position.js'
 import Header from './components/Header.js'
 
 function App() {
-  function handleClick() {
-    window.fetch('/position/scraper')
-  }
 
   const [allPosition, setAllPosition] = React.useState([])
+  const [refresh, setRefresh] = React.useState(false)
+  console.log(refresh)
 
   React.useEffect(() => {
     window.fetch('/position')
       .then(response => response.json())
       .then(data => setAllPosition(data))
-  }, [handleClick])
+  }, [refresh])
 
   const positions = allPosition.map(position => {
     return (
@@ -25,11 +24,18 @@ function App() {
     )
   })
 
+  function handleClick() {
+    window.fetch('/position/scraper')
+      .then(response => response.json())
+      .then(data => setRefresh(data))
+
+  }
+
   return (
     <div>
       <button onClick={handleClick}> New search </button>
       <table>
-        <Header/>
+        <Header />
         <tbody>
           {positions}
         </tbody>
